82b4ba1de5eb4d31282d6084ffa00cf9f647afd5
uduwela96 <puduwel@mitrai.com>
2022-09-04 01:26:12 +0530

    contoller , driver and model completd

---
+++ controllers/book.go
@@ -0,0 +1,89 @@
+package controllers
+
+import (
+	"database/sql"
+	"encoding/json"
+	"gangashinienovels_backend/models"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Controller struct{}
+
+var books []models.Book
+
+func logFatal(err error) {
+	if err != nil {
+		logFatal(err)
+	}
+}
+
+func (c Controller) GetBooks(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		books = []models.Book{}
+
+		rows, err := db.Query("Select * from books")
+		logFatal(err)
+		defer rows.Close()
+
+		for rows.Next() {
+			err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year, &book.Description, &book.Thumbnail)
+			logFatal(err)
+			books = append(books, book)
+		}
+		json.NewEncoder(w).Encode(books)
+	}
+}
+
+func (c Controller) GetBook(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		params := mux.Vars(r)
+
+		rows := db.QueryRow("select * from books where id=$1", params["id"])
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year, &book.Description, &book.Thumbnail)
+		logFatal(err)
+		json.NewEncoder(w).Encode(book)
+	}
+}
+
+func (c Controller) AddBook(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		var bookID int
+
+		json.NewDecoder(r.Body).Decode(&book)
+		err := db.QueryRow("insert into books (title , author, year ,description, thumbnail) values ($1,$2,$3,$4,$5) RETURNING id",
+			book.Title, book.Author, book.Year, book.Description, book.Thumbnail).Scan(&bookID)
+		logFatal(err)
+		json.NewEncoder(w).Encode(bookID)
+	}
+}
+
+func (c Controller) UpdateBook(db *sql.DB) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		var book models.Book
+		json.NewDecoder(r.Body).Decode(&book)
+		result, err := db.Exec("update books set title=$1,author=$2,year=$3,description=$4,thumbnail=$5 where id=$6 RETURNING id", book.Title, book.Author, book.Year, book.Description, book.Thumbnail, book.ID)
+		rowsUpdated, err := result.RowsAffected()
+		logFatal(err)
+		//return no of rows updated
+		json.NewEncoder(w).Encode(rowsUpdated)
+	}
+}
+
+func (c Controller) RemoveBook(db *sql.DB) http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		params := mux.Vars(r)
+	
+		result, err := db.Exec("delete from books where id=$1", params["id"])
+		logFatal(err)
+		rowsDeleted, err := result.RowsAffected()
+		logFatal(err)
+		json.NewEncoder(w).Encode(rowsDeleted)
+	}
+}
+
+
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"database/sql"
 	_ "database/sql"
-	"encoding/json"
-	"gangashinienovels_backend/models"
+	"gangashinienovels_backend/controllers"
 	"gangashinienovels_backend/driver"
+	"gangashinienovels_backend/models"
+
 	"log"
 	"net/http"
 
@@ -30,67 +31,13 @@ func logFatal(err error) {
 
 func main() {
 	db = driver.ConnectDB()
+	controller := controllers.Controller{}
 
 	router := mux.NewRouter()
-	router.HandleFunc("/books", getBooks).Methods("GET")
-	router.HandleFunc("/books/{id}", getBook).Methods("GET")
-	router.HandleFunc("/books", addBook).Methods("POST")
-	router.HandleFunc("/books", updateBook).Methods("PUT")
-	router.HandleFunc("/books/{id}", removeBook).Methods("DELETE")
+	router.HandleFunc("/books", controller.GetBooks(db)).Methods("GET")
+	router.HandleFunc("/books/{id}", controller.GetBook(db)).Methods("GET")
+	router.HandleFunc("/books", controller.AddBook(db)).Methods("POST")
+	router.HandleFunc("/books", controller.UpdateBook(db)).Methods("PUT")
+	router.HandleFunc("/books/{id}", controller.RemoveBook(db)).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8081", router))
-}
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
-	books = []models.Book{}
-
-	rows, err := db.Query("Select * from books")
-	logFatal(err)
-	defer rows.Close()
-
-	for rows.Next() {
-		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year, &book.Description, &book.Thumbnail)
-		logFatal(err)
-		books = append(books, book)
-	}
-	json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
-	params := mux.Vars(r)
-
-	rows := db.QueryRow("select * from books where id=$1", params["id"])
-	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year, &book.Description, &book.Thumbnail)
-	logFatal(err)
-	json.NewEncoder(w).Encode(book)
-}
-
-func addBook(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
-	var bookID int
-
-	json.NewDecoder(r.Body).Decode(&book)
-	err := db.QueryRow("insert into books (title , author, year ,description, thumbnail) values ($1,$2,$3,$4,$5) RETURNING id",
-		book.Title, book.Author, book.Year, book.Description, book.Thumbnail).Scan(&bookID)
-	logFatal(err)
-	json.NewEncoder(w).Encode(bookID)
-}
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	var book models.Book
-	json.NewDecoder(r.Body).Decode(&book)
-	result, err := db.Exec("update books set title=$1,author=$2,year=$3,description=$4,thumbnail=$5 where id=$6 RETURNING id", book.Title, book.Author, book.Year, book.Description, book.Thumbnail, book.ID)
-	rowsUpdated, err := result.RowsAffected()
-	logFatal(err)
-	//return no of rows updated
-	json.NewEncoder(w).Encode(rowsUpdated)
-}
-func removeBook(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-
-	result, err := db.Exec("delete from books where id=$1", params["id"])
-	logFatal(err)
-	rowsDeleted, err := result.RowsAffected()
-	logFatal(err)
-	json.NewEncoder(w).Encode(rowsDeleted)
 }
